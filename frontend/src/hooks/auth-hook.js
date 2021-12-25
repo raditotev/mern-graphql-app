b81eddf616cfb0e91f8626b5fa932410b29246aa
Radi Totev <raditotev@gmail.com>
2021-12-25 10:20:10 +0200

    Move auth hook in frontend folder
    It was placed by mistake inside the root of the project

--- frontend/src/hooks/auth-hook.js
+++ frontend/src/hooks/auth-hook.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { AuthContext } from '../frontend/src/context/auth-context';
+import { AuthContext } from '../context/auth-context';
 
 const useAuth = (props) => {
   const authContext = useContext(AuthContext);
