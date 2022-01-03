4bbd5478269af267b81b5d26cf3b7426a77afe0e
Radi Totev <raditotev@gmail.com>
2022-01-03 11:43:30 +0200

    Enable user to add new events from EventsPage

--- frontend/src/pages/EventsPage.js
+++ frontend/src/pages/EventsPage.js
@@ -1,5 +1,35 @@
+import { useState } from 'react';
+
+import Modal from '../components/UI/Modal';
+import NewEvent from '../components/Events/NewEvent';
+
+import './EventsPage.css';
+
 const EventsPage = () => {
-  return <h1>Events Page</h1>;
+  const [showModal, setShowModal] = useState(false);
+
+  const showModalHandler = () => {
+    setShowModal(true);
+  };
+
+  const hideModalHandler = () => {
+    setShowModal(false);
+  };
+  return (
+    <>
+      {showModal ? (
+        <Modal title="Create new event" onCancel={hideModalHandler}>
+          <NewEvent closeModal={hideModalHandler} />
+        </Modal>
+      ) : null}
+      <div className="events-page">
+        <h1>Share new event with your friends</h1>
+        <button className="btn" type="button" onClick={showModalHandler}>
+          Create event
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default EventsPage;
