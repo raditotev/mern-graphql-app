c0ada0aa9ed86ef6f9f9e0713b2bf4ddad439d1b
Radi Totev <raditotev@gmail.com>
2022-01-04 15:49:42 +0200

    Display bookings on bookings page

--- frontend/src/pages/BookingsPage.js
+++ frontend/src/pages/BookingsPage.js
@@ -1,5 +1,60 @@
+import { useEffect, useState } from 'react';
+import Spinner from '../components/UI/Spinner';
+import { sendQuery } from '../helpers/client';
+import useAuth from '../hooks/auth-hook';
+
 const BookingsPage = () => {
-  return <h1>Bookings Page</h1>;
+  const [bookings, setBookings] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const { token } = useAuth();
+
+  useEffect(() => {
+    const fetchBookings = async () => {
+      const query = {
+        query: `
+          query {
+            bookings {
+              _id
+              createdAt
+              event {
+                title
+                date
+                creator {
+                  _id
+                }
+              }
+            }
+          }
+        `,
+      };
+      const data = await sendQuery(query, {
+        Authorization: `Bearer ${token}`,
+      });
+      setBookings(data.bookings);
+    };
+
+    setIsLoading(true);
+
+    try {
+      fetchBookings();
+    } catch (error) {
+      setIsLoading(false);
+      console.log(error);
+    }
+    setIsLoading(false);
+  }, [token]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  return (
+    <ul>
+      {bookings.map((booking) => (
+        <li key={booking._id}>{booking.event.title}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default BookingsPage;
