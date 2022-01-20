e54a5c648a1b70977e42fdfdb756337d775a5bc6
Radi Totev <raditotev@gmail.com>
2022-01-20 11:06:48 +0200

    Logout user after token expiration

--- frontend/src/context/auth-context.js
+++ frontend/src/context/auth-context.js
@@ -1,28 +1,57 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 
 const AuthContext = createContext();
+
+let expirationTimer;
 
 const AuthContextProvider = ({ children }) => {
   const [token, setToken] = useState(null);
   const [userId, setUserId] = useState(null);
-  const [expirationTime, setExpirationTime] = useState(null);
+  const isLoggedIn = !!token;
 
   const login = (token, uid, expiration) => {
     setToken(token);
     setUserId(uid);
-    setExpirationTime(expiration);
+    const expirationTime = new Date().getTime() + expiration * 60 * 60 * 1000;
+    localStorage.setItem('__easy_event__token__', token);
+    localStorage.setItem('__easy_event__uid__', uid);
+    localStorage.setItem('__easy_event___expiration_time__', expirationTime);
   };
 
   const logout = () => {
     setToken(null);
     setUserId(null);
-    setExpirationTime(null);
+    localStorage.removeItem('__easy_event__token__');
+    localStorage.removeItem('__easy_event__uid__');
+    localStorage.removeItem('__easy_event___expiration_time__');
   };
 
+  useEffect(() => {
+    const token = localStorage.getItem('__easy_event__token__');
+    if (!token) {
+      return;
+    }
+    const uid = localStorage.getItem('__easy_event__uid__');
+    const expirationTime = localStorage.getItem(
+      '__easy_event___expiration_time__'
+    );
+    const remainingTime = expirationTime - new Date().getTime();
+
+    if (remainingTime < 10000) {
+      logout();
+    }
+    expirationTimer = setTimeout(logout, remainingTime);
+
+    setToken(token);
+    setUserId(uid);
+
+    return () => {
+      clearTimeout(expirationTimer);
+    };
+  }, []);
+
   return (
-    <AuthContext.Provider
-      value={{ token, userId, login, logout, isLoggedIn: !!token }}
-    >
+    <AuthContext.Provider value={{ token, userId, login, logout, isLoggedIn }}>
       {children}
     </AuthContext.Provider>
   );
